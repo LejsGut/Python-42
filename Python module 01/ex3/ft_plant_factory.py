d5b2c20241705669adbd8c67154443912fcc8d3b
LejsGut <lejssalkic572@gmail.com>
2026-01-19 02:04:15 +0100

    managed to make and accsess a list in python. still not comfy with the syntax

---
+++ Python module 01/ex3/ft_plant_factory.py
@@ -0,0 +1,30 @@
+class Plant:
+    def __init__(self, name, starting_height, starting_age, amount_plants):
+        self.name = name
+        self.starting_height = starting_height
+        self.starting_age = starting_age
+        self.amount_plants = amount_plants
+
+    def info(self, count):
+        print(f"Created: {self.name} ({self.starting_height}cm, {self.starting_age} days)")
+
+    def how_many_created(self, count):
+        print(f"Total plants created: {count}")
+
+
+Rose = Plant("Rose", 25, 30, 5)
+Oak = Plant("Oak", 200, 365, 5)
+Cactus = Plant("Cactus", 5, 90, 5)
+Sunflower = Plant("Sunflower", 80, 45, 5)
+Fern = Plant("Fern", 15, 120, 5)
+
+print("=== Plant Factory Output ===")
+plants = [Rose, Oak, Cactus, Sunflower, Fern]
+count = 0
+while count < Rose.amount_plants:
+    plants[count].info(count)
+    count += 1
+print("")
+Rose.how_many_created(count)
+
+
