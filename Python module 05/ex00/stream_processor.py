1c7884cb7ab892b9546734adbe8348400a1fdb00
lejs <lsalkic@student.42heilbronn.de>
2026-03-03 18:23:06 +0100

    tomorrow more

--- Python module 05/ex00/stream_processor.py
+++ Python module 05/ex00/stream_processor.py
@@ -1,9 +1,25 @@
 
 from abc import ABC, abstractmethod
+from typing import Any
 
 
 class DataProcessor(ABC):
 
     @abstractmethod
-    def process(self, data: str) -> str:
+    def process(self, data: Any) -> str:
         pass
+
+class NumericProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        try:
+            for num in data:
+                if not isinstance(num, (int, float)):
+                    raise ValueError(f"Invalid data: {num} is not a number")
+                return False
+        except TypeError:
+            raise ValueError("Invalid data: expected a number or a list of numbers")
+        return True
+
+    def process(self, data: Any) -> str:
+        print("Initialize Numeric Processor....")
+        print(f"Processing data: {data}")
